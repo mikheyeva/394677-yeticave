69fffec00f216eef69278bb532ec241048209e8d
mikheyeva <algaeminor@gmail.com>
2018-11-28 17:18:22 +0300

    схемы базы данных
    схемы базы данных

---
+++ schema.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE yeticave
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci
+
+CREATE TABLE categories(
+`id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+`name` char (255) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `name` (`name`)
+);
+
+CREATE TABLE lots(
+`id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+`author_id` INT unsigned NOT NULL,
+`winner_id` INT unsigned NOT NULL,
+`category_id` INT unsigned NOT NULL,
+`name` char (255) NOT NULL,
+`description` text,
+`url-image` char (255),
+`start-price`  INT unsigned NOT NULL,
+`dt_finish` timestamp default current_timestamp NOT NULL,
+`bet_step` INT unsigned NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `author_id` (`author_id`),
+UNIQUE KEY `winner_id` (`winner_id`),
+UNIQUE KEY `category_id` (`category_id`)
+);
+
+CREATE TABLE wager(
+`id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+`user_id` INT(11) unsigned NOT NULL,
+`lot_id` INT(11) unsigned NOT NULL,
+`dt_placing` timestamp default current_timestamp NOT NULL,
+`user_amount` INT unsigned NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `user_id` (`user_id`),
+UNIQUE KEY `lot_id` (`lot_id`)
+)
+
+CREATE TABLE users(
+`id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+`created_lots_id` INT(11) unsigned NOT NULL,
+`wager_id` INT(11) unsigned NOT NULL,
+`dt_add` timestamp default current_timestamp NOT NULL,
+`email` char (128) NOT NULL,
+`name` char (255) NOT NULL,
+`password` char(64) NOT NULL,
+`avatar_path` char(255),
+`token` char(32),
+PRIMARY KEY (`id`),
+UNIQUE KEY `created_lots_id` (`created_lots_id`),
+UNIQUE KEY `wager_id` (`wager_id`),
+UNIQUE KEY `email` (`email`)
+);
+
+
