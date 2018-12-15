2b1b06cffddf24ade0fa2290b7f46391613e1152
mikheyeva <algaeminor@gmail.com>
2018-12-15 18:19:38 +0300

    исправлена текущая цена
    исправлена текущая цена , теперь с учетом ставки

--- lot.php
+++ lot.php
@@ -30,22 +30,44 @@ JOIN categories c
 ON l.category_id = c.id
 WHERE l.`id` = '" . $lotId . "'";
 
+$sql2 = "SELECT max(user_amount) as amount
+FROM wager
+WHERE lot_id = '" . $lotId . "'";
+
 $lot = null;
 $result = mysqli_query($link, $sql);
 $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-//echo '<pre>';
-//var_dump($lots);
-//echo '</pre>';
-//die();
+$wager = null;
+$max_amount = null;
+$result2 = mysqli_query($link, $sql2);
+$wagers = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
 if (!empty($lots[0])) {
     $lot = $lots[0];
 }
+
+if (!empty($wagers[0])) {
+    $wager = $wagers[0];
+    $max_amount = $wager['amount'];
+}
+
+if (!empty($max_amount)) {
+    $current_price = formatted_price(htmlspecialchars($max_amount));
+} else {
+    $current_price = formatted_price(htmlspecialchars($lot['start_price']));
+}
+
+//echo '<pre>';
+//var_dump($max_amount);
+//echo '</pre>';
+//die();
 
 echo include_template('lot.php', [
     'categories' => $categories,
     'sql' => $sql,
     'lot' => $lot,
     'formatted_time' => $formatted_time,
+    'max_amount' => $max_amount,
+    'current_price' => $current_price,
 ]);
